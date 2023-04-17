f2b7cb04a697870654c531c4249e0fac196b5536
Tammy <126425884+ChickenTamales@users.noreply.github.com>
2023-04-17 09:30:21 -0600

    pushing week 5 coding assignment to GitHub

---
+++ App.java
@@ -0,0 +1,18 @@
+package week5CodingAssignment;
+
+public class App {
+
+public static void main(String[] args) {
+
+	    static AsteriskLogger.log("I don't know");
+	    static AsteriskLogger.error("Cannot compute!");
+
+	    static SpacedLogger.log("Ugggghhhh");
+	    static SpacedLogger.error("Why?!");
+
+
+}
+	
+}	
+
+}
---
+++ Logger.java
@@ -0,0 +1,5 @@
+package week5CodingAssignment;
+
+public interface Logger {
+
+}
